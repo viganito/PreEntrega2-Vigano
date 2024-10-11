38434245024afba71eb66491e050fbf889a48bfe
vigaiv <ivanvigano13@gmail.com>
2024-10-11 19:32:27 -0300

    proyecto-final

--- src/service/config.js
+++ src/service/config.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDEnhGys90A61qvPtJvDRS3ZZcQuqv62cE",
+  apiKey: import.meta.env.VITE_BOOTSTRAP_API_KEY,
   authDomain: "proyecto-final-8b4a9.firebaseapp.com",
   projectId: "proyecto-final-8b4a9",
   storageBucket: "proyecto-final-8b4a9.appspot.com",
